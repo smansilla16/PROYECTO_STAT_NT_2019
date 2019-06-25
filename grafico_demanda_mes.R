a231de3783544d8b44fc14f3698edb9fdb9ba4f3
dpenza <dpenza@vera.com.uy>
2019-06-25 09:13:13 -0300

    Grafico demanda mes y ajustes estéticos

---
+++ grafico_demanda_mes.R
@@ -0,0 +1,42 @@
+source("carga_datos_clima_consumo.R", encoding = "UTF-8")
+
+grafico.titulo <- "Grafico de Demanda - Meses"
+
+grafico.descripcion <- paste("Grafico de línea de la demanda según el mes del año,",
+                             "para el período comprendido entre el", ajustes$rango[1],
+                             "y el", ajustes$rango[2],
+                             ". La demanda en el gráfico se resume como la media",
+                             "de la energía eléctrica diaria que se destina al país",
+                             "respecto de cada mes, y es expresada en GWh.")
+
+grafico.observacion <- paste("Notamos que en promedio existe mayor consumo diaro primero entre junio y agosto,",
+                             "y luego entre diciembre y febrero.")
+
+dmconsumo <- consumoEE.datos3 %>%
+  
+  filter(Fecha >= ajustes$rango[1] & Fecha <= ajustes$rango[2]) %>%
+
+  group_by(Mes = month(Fecha, label = TRUE, abbr = FALSE)) %>%
+  
+  summarise(Demanda = mean(Demanda))
+
+splconsumo <- as.data.frame(spline(dmconsumo$Mes, dmconsumo$Demanda))
+
+grafico <- dmconsumo %>%
+
+  ggplot(aes(x = Mes,
+             y = Demanda, group = 1)) +
+  
+  geom_point() +
+
+  geom_line(data = splconsumo, aes(x = x, y = y), color="blue") +
+
+  theme(aspect.ratio = 1,
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5)) +
+  
+  labs(x = "Meses del año",
+       y = "Demanda de energia promedio (GWh)",
+       title = "¿Como se comporta la demanda de energia según los meses del año?")
+
+rm(dmconsumo, splconsumo)
