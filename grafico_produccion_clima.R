84d7181cc8ff269bcf667602a354106da7c55c57
FranciscoBM <franbonora@hotmail.com>
2019-06-26 17:02:28 -0300

    Modificacion grafico_produccion_clima. Si se borra "facet_grid(~Fuente)+" queda el grafico apilado de barras

--- grafico_produccion_clima.R
+++ grafico_produccion_clima.R
@@ -15,22 +15,23 @@ grafico.observacion <- paste("Se observa una caída en la producción eléctrica
 
 grafico <- 
   consumoEE.datos3 %>% 
-
+  
   group_by(month = floor_date(Fecha, agrupacion.texto2text(ajustes$agrupacion)), Fuente) %>%
-
+  
   filter(Producción!=0, Fecha >= ajustes$rango[1] & Fecha <= ajustes$rango[2]) %>%
-
-  summarise(ProducciónPromedio = mean(Producción),
-            TempMedia=mean(temp_c)) %>% 
-
-  ggplot(aes(x = TempMedia,
-             y = ProducciónPromedio,
-             color = Fuente)) +
-
-  geom_point() + geom_smooth(method="lm",se=FALSE) +
-
+  
+  summarise(Producción = mean(Producción),
+            temp_c=mean(temp_c)) %>% 
+  group_by(Mes = month(month, label = TRUE, abbr = FALSE), Fuente)%>%
+  summarise(Producción = mean(Producción),
+            temp_c=mean(temp_c))%>%
+  ggplot(aes(x = Mes,
+             y = Producción,
+             fill = Fuente)) +
+  geom_bar(stat = "identity")+
+  facet_grid(~Fuente)+
+  theme(axis.text.x = element_text(angle = 90))+
+  
   labs(x=paste("Temperatura media", ajustes$agrupacion,"(ºC)"),
        y=paste("Producción energética media", ajustes$agrupacion ,"/ 100 (GWh)"),
-       color="Fuente") +
-
-   theme_grey() + theme(aspect.ratio = 1)
+       color="Fuente") 
