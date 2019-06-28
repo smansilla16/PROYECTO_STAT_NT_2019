beac380a95153cb9e0135ce465af006ae4c967b1
dpenza <dpenza@vera.com.uy>
2019-06-28 06:06:22 -0300

    Instalador de paquetes

---
+++ instala_librerias.R
@@ -0,0 +1,27 @@
+instala.librerias <- function(...)
+{
+  for(l in list(...))
+  {
+     if(! l %in% rownames(installed.packages()))
+     {
+       cat("Instalando paquete que faltaba:", l, "\n")
+       install.packages(l)
+     }
+  }
+}
+
+instala.librerias(
+  "tidyverse",
+  "ggplot2",
+  "ggpmisc",
+  "readxl",
+  "forcats",
+  "lubridate",
+  "here",
+  "shiny",
+  "shinydashboard",
+  "shinyWidgets",
+  "cowplot"
+)
+
+
